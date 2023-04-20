de3aaf9706eef9c02da045842303cda8571be8b4
Joanne <G00283773@atu.ie>
2023-04-20 22:51:46 +0100

    pands project

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# analysis.py
+# Author: Joanne Feeney
+# This program outputs a summary of each variable to a single text file
+# Saves a histogram of each variable to png files, and  
+# Outputs a scatter plot of each pair of variables 
+# Performs any other analysis you think is appropriate  
