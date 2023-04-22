dc501ce1699883cf58c5172541aad5c34b01406e
Joanne <G00283773@atu.ie>
2023-04-22 19:11:20 +0100

    adding iris data set and reading file code

--- analysis.py
+++ analysis.py
@@ -4,3 +4,9 @@
 # Saves a histogram of each variable to png files, and  
 # Outputs a scatter plot of each pair of variables 
 # Performs any other analysis you think is appropriate  
+
+# Opening and reading the dataset
+# https://www.w3schools.com/python/python_file_open.asp
+f = open("iris.data", "r")
+print(f.read())
+
