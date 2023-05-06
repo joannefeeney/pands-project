e5ef72d07ace50b3437dbcf82334b4d95393caea
Joanne <G00283773@atu.ie>
2023-05-06 17:16:07 +0100

    inserting code from geeksforgeeks

--- analysis.py
+++ analysis.py
@@ -10,15 +10,39 @@ import pandas as pd
 
 data = pd.read_csv("irisdata.csv")
 
-#setosa data 
+# setosa data 
 # print(data[0:49])
 
-#versicolor data 
+# versicolor data 
 # print(data[50:99])
 
-#verginica data 
+# verginica data 
 # print(data[100:151])
 
 # you can also save it in a variable for further use in analysis
-sliced_data=data[50:99]
-print(sliced_data)
+# sliced_data=data[50:99]
+# print(sliced_data)
+
+# adding column name to the respective columns
+# Code source:
+# https://www.geeksforgeeks.org/add-column-names-to-dataframe-in-pandas/
+data.columns =['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidthth', 'Species']
+
+
+# displaying only certain columns
+# save it in a another variable named "specific_data" e.g.
+# specific_data=data[["SepalLength","Species"]]
+# data[["column_name1","column_name2","column_name3"]]
+# now we will print the first 10 columns of the specific_data dataframe e.g.
+# print(specific_data.head(10))
+
+# calculating sum, mean and mode of a particular column
+# data["column_name"].sum()
+''' 
+sum_data = data["SepalLength"].sum()
+mean_data = data["SepalLength"].mean()
+median_data = data["SepalLength"].median()
+
+print(f"Data for the length of all iris species: \n""Sum:",sum_data, "\nMean:", mean_data, "\nMedian:",median_data)
+'''
+
